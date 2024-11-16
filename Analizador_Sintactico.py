73f6b6f8ba22d683b04c4606115c0e81c37a9911
emjorome <emjorome@espol.edu.ec>
2024-11-16 15:56:16 -0500

    funcion para guardar log, entre otros

--- Analizador_Sintactico.py
+++ Analizador_Sintactico.py
@@ -1,4 +1,5 @@
 import ply.yacc as yacc
+import datetime
 
 # Get the token map from the lexer.  This is required.
 from Analizador_Lexico import tokens
@@ -14,18 +15,35 @@ def p_sentencias(p):
                 | comparacionNumero 
                 | comparacionBool'''
 
+#Guardar datos
+now = datetime.datetime.now()
+usuario = "emjorome"
+log_filename = f"sintactico-{usuario}-{now.strftime('%d%m%Y-%Hh%M')}.txt"
+
+def guardar_log(mensaje):
+    """Escribe un mensaje en el archivo de log."""
+    with open(log_filename, 'a') as log_file:
+        log_file.write(mensaje + '\n')
+
 # Error rule for syntax errors
 def p_error(p):
-    print("Error de sintaxis en la linea %d!" % p.lineno)
+    msg_error = "Error de sintaxis en la linea %d!" % p.lineno
+    print(msg_error)
+    guardar_log(msg_error)
 
 # Build the parser
 parser = yacc.yacc()
 
 while True:
    try:
-       s = input('calc > ')
+       s = input('kotlin > ')
    except EOFError:
        break
    if not s: continue
    result = parser.parse(s)
+
+   if result:
+        mensaje = f"Entrada: {s}\nResultado: Correcto\n"
+
    print(result)
+   guardar_log(mensaje)
